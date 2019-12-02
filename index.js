7fe0f0e65d9a04159917a6007ca70743206a8334
TopDev <k100cool@outlook.com>
2019-12-02 18:56:52 -0500

    initial version

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Pusher = require('pusher');
+const app = express();
+
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Session middleware
+
+// Create an instance of Pusher
+const pusher = new Pusher({
+    appId: '910661',
+    key: 'c6dc441af056a7e3f1bf',
+    secret: '0177409ddf70c5520e0f',
+    cluster: 'mt1',
+    encrypted: true
+});
+
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/index.html');
+});
+
+// get authentictation for the channel;
+app.post('/pusher/auth', (req, res) => {
+        const socketId = req.body.socket_id;
+        const channel = req.body.channel_name;
+        var presenceData = {
+            user_id: Math.random().toString(36).slice(2) + Date.now()
+        }
+        const auth = pusher.authenticate(socketId, channel, presenceData);
+        res.send(auth);
+});
+
+//listen on the app
+app.listen(3000, () => {
+    return console.log('Server is up on 3000')
+});
